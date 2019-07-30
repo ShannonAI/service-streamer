e117dd111cda940fd1322f29b963545ffb590aef
liuxin <lxhustauto@gmail.com>
2019-07-30 16:00:12 +0800

    try add subprocess worker & support multi gpu workers

--- example/example.py
+++ example/example.py
@@ -4,9 +4,10 @@
 import torch
 import time
 import os
+from tqdm import tqdm
 from pytorch_transformers import *
-from service_streamer import ThreadedStreamer as Streamer
-from service_streamer import StreamWorker, GpuWorkerManager
+from service_streamer import ThreadedStreamer, Streamer
+from service_streamer import RedisWorker, GpuWorkerManager
 
 
 class Model(object):
@@ -17,7 +18,7 @@ class Model(object):
         self.tokenizer = BertTokenizer.from_pretrained(self.model_path)
         self.bert = BertModel(self.config)
         self.bert.eval()
-        self.bert.to("cuda")
+        # self.bert.to("cuda")
 
     def predict(self, batch):
         batch_inputs = []
@@ -35,25 +36,32 @@ class Model(object):
 
 
 def main():
+    max_batch = 128
     model = Model()
     text = "Who was Jim Henson ? Jim Henson was a puppeteer"
-    streamer = Streamer(model.predict, 1000, 0.1)
-    num_times = 1000
+    streamer = Streamer(model.predict, batch_size=max_batch, max_latency=0.1, worker_num=2, cuda_devices=(0, 1), model=model)
+    # streamer = ThreadedStreamer(model.predict, batch_size=max_batch, max_latency=0.1)
+    num_times = 3000
 
     class GpuWorkers(GpuWorkerManager):
 
         def gpu_worker(self, index, gpu_num):
             os.environ["CUDA_VISIBLE_DEVICES"] = str(index % gpu_num)
-            StreamWorker(model.predict, 64, max_latency=0.1).run_forever()
+            RedisWorker(model.predict, 64, max_latency=0.1).run_forever()
 
+    """
     t_start = time.time()
-    for i in range(num_times):
+    for i in tqdm(range(num_times)):
         output = model.predict([text])
     t_end = time.time()
     print('model prediction time', t_end - t_start)
+    """
 
     t_start = time.time()
-    output = model.predict([text] * num_times)
+    inputs = [text] * num_times
+    for i in tqdm(range(num_times // max_batch + 1)):
+        output = model.predict(inputs[i*max_batch:(i+1)*max_batch])
+        print(len(output))
     t_end = time.time()
     print('model batch prediction time', t_end - t_start)
 
@@ -62,13 +70,16 @@ def main():
     for i in range(num_times):
         future = streamer.submit([text])
         xs.append(future)
-    for future in xs:  # 先拿到所有future对象，再等待异步返回
-        output = future.result()
+
+    for future in tqdm(xs):  # 先拿到所有future对象，再等待异步返回
+        output = future.result(timeout=20)
     t_end = time.time()
     print('streamer prediction time', t_end - t_start)
 
-    GpuWorkers().run_workers_forever(worker_num=8, gpu_num=4)
+    # streamer._worker_process.join()
+    # GpuWorkers().run_workers_forever(worker_num=8, gpu_num=4)
 
 
 if __name__ == '__main__':
     main()
+
