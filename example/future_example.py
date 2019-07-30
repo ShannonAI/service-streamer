5225e7eb7dbe3a7fc8e5111043c68db1943a89c3
liuxin <lxhustauto@gmail.com>
2019-07-30 21:01:20 +0800

    use bert lm as example

--- example/future_example.py
+++ example/future_example.py
@@ -1,47 +1,21 @@
 # coding=utf-8
 # Created by Meteorix at 2019/7/22
 
-import torch
 import time
 import os
 from tqdm import tqdm
-from pytorch_transformers import *
 from service_streamer import ThreadedStreamer, Streamer
 from service_streamer import RedisWorker, GpuWorkerManager
-
-
-class Model(object):
-    def __init__(self):
-        self.model_path = "bert-base-uncased"
-        self.model_path = "/data/nfsdata/nlp/BERT_BASE_DIR/uncased_L-24_H-1024_A-16"
-        self.config = BertConfig.from_pretrained(self.model_path)
-        self.tokenizer = BertTokenizer.from_pretrained(self.model_path)
-        self.bert = BertModel(self.config)
-        self.bert.eval()
-        # self.bert.to("cuda")
-
-    def predict(self, batch):
-        batch_inputs = []
-        for text in batch:
-            tokenized_text = self.tokenizer.tokenize(text)
-            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-            batch_inputs.append(indexed_tokens)
-
-        tokens_tensor = torch.tensor(batch_inputs).to("cuda")
-
-        with torch.no_grad():
-            encoded_layers = self.bert(tokens_tensor)[0]
-
-        return encoded_layers.tolist()
+from bert_model import Model
 
 
 def main():
     max_batch = 128
     model = Model()
     text = "Who was Jim Henson ? Jim Henson was a puppeteer"
-    streamer = Streamer(model.predict, batch_size=max_batch, max_latency=0.1, worker_num=2, cuda_devices=(0, 1), model=model)
+    streamer = Streamer(model.predict, batch_size=max_batch, max_latency=0.1, worker_num=4, cuda_devices=(0, 1, 2, 3), model=model)
     # streamer = ThreadedStreamer(model.predict, batch_size=max_batch, max_latency=0.1)
-    num_times = 3000
+    num_times =13000
 
     class GpuWorkers(GpuWorkerManager):
 
@@ -56,7 +30,7 @@ def main():
     t_end = time.time()
     print('model prediction time', t_end - t_start)
     """
-
+    model.bert.to("cuda")
     t_start = time.time()
     inputs = [text] * num_times
     for i in tqdm(range(num_times // max_batch + 1)):
