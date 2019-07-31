8ac5894402918756a43bee15e2d067a0fbf92f9d
liuxin <lxhustauto@gmail.com>
2019-07-31 17:16:30 +0800

    fix readme & setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 # encoding: utf-8
 from setuptools import setup, find_packages
 
-VERSION = {"VERSION": '0.1'}
 
 setup(name='service_streamer',
-      version=VERSION["VERSION"],
+      version="0.0.1",
       description='service_streamer',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: CPython',
           'Operating System :: OS Independent',
       ],      
       keywords='service_streamer',
       url='https://gitlab.shannonai.com/nlp/basic_info_extraction',
       license='Apache',
-      packages=find_packages(exclude=["*.tests", "*.tests.*",
-                                      "tests.*", "tests"]),
+      packages=find_packages(exclude=["example"]),
       install_requires=[
           'redis',
           'tqdm',
       ],
       include_package_data=True,
       python_requires='>=3.6.1',
-      zip_safe=False)
+      zip_safe=False
+)
