5d0ff76fc23f5b29a87d5bdb75f9419c350193a1
liuxin <lxhustauto@gmail.com>
2019-08-03 17:43:07 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 # encoding: utf-8
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(name='service_streamer',
       version="0.0.1",
-      description='service_streamer',
+      description='Boosting your web service of deep learning applications',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       classifiers=[
           'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: CPython',
           'Operating System :: OS Independent',
       ],      
       keywords='service_streamer',
-      url='https://gitlab.shannonai.com/nlp/basic_info_extraction',
-      license='Apache',
+      url='https://github.com/shannonAI',
       packages=find_packages(exclude=["example"]),
       install_requires=[
           'redis',
           'tqdm',
       ],
       include_package_data=True,
-      python_requires='>=3.6.1',
+      python_requires='>=3.5',
       zip_safe=False
 )
