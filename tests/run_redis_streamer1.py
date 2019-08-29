a820a026d5ac5e12be8a128e9ca7b7f1aa18a02b
Ziqi Wang <ziqi_wang@shannonai.com>
2019-08-28 22:43:32 -0400

    remove useless file

--- tests/run_redis_streamer1.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from multiprocessing import freeze_support
-from service_streamer import run_redis_workers_forever
-from example.bert_model import ManagedBertModel
-from tests.test_service_streamer import ManagedVisionModel
-
-if __name__ == "__main__":
-    freeze_support()
-    run_redis_workers_forever(ManagedVisionModel, 64, 0.1, worker_num=4, cuda_devices=(1,), prefix='test')
